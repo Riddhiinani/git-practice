50981a957144ca3b52824d8ea17869a201550fc6
Riddhi Inani <riddhiinani1012@gmail.com>
2026-08-05 11:29:23 +0530

    Added student.json and updated test.js

--- test.js
+++ test.js
@@ -19,9 +19,17 @@ else{
 }
 
 if(fs.existsSync("script.js")){
-    console.log("TC-02:script.js exists : PASS");
+    console.log("TC-03:script.js exists : PASS");
 }
 else{
-  console.log("TC-02:script.js exists : FAIL");
+  console.log("TC-03:script.js exists : FAIL");
+  passed=false;  
+}
+
+if(fs.existsSync("student.json")){
+    console.log("TC-04:student.json exists : PASS");
+}
+else{
+  console.log("TC-04:student.json exists : FAIL");
   passed=false;  
 }
